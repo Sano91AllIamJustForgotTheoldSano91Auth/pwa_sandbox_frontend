4cef5cc4bff07dd0aa4a1291e276df26d6b9e2cf
Sano91 <sano.kulin@emed4all.com>
2025-09-29 14:45:20 +0200

    auto update for all users when deploy to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,63 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+// import { VitePWA } from 'vite-plugin-pwa'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//     VitePWA({
+//       registerType: 'autoUpdate',
+//       includeAssets: [
+//         'favicon.ico',
+//         'apple-touch-icon.png',
+//         'apple-touch-icon-152x152.png',
+//         'apple-touch-icon-167x167.png',
+//       ],
+//       manifest: {
+//         name: 'PWA Sandbox',
+//         short_name: 'Sandbox',
+//         description: 'A Vue + Vite PWA sandbox project',
+//         theme_color: '#ffffff',
+//         background_color: '#ffffff',
+//         start_url: '.',
+//         display: 'standalone',
+//         icons: [
+//           {
+//             src: 'pwa-192x192.png',
+//             sizes: '192x192',
+//             type: 'image/png',
+//           },
+//           {
+//             src: 'pwa-256x256.png',
+//             sizes: '256x256',
+//             type: 'image/png',
+//           },
+//           {
+//             src: 'pwa-384x384.png',
+//             sizes: '384x384',
+//             type: 'image/png',
+//           },
+//           {
+//             src: 'pwa-512x512.png',
+//             sizes: '512x512',
+//             type: 'image/png',
+//           },
+//         ],
+//       },
+//     }),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+// })
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -11,7 +71,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'autoUpdate', // ensures the SW checks for updates automatically
       includeAssets: [
         'favicon.ico',
         'apple-touch-icon.png',
@@ -48,6 +108,11 @@ export default defineConfig({
             type: 'image/png',
           },
         ],
+      },
+      workbox: {
+        // These two lines make the new SW activate instantly:
+        skipWaiting: true,
+        clientsClaim: true,
       },
     }),
   ],
